6b8b4278626248412415b0a92ac337b340109529
Dhvani <dhvani16patel02@gmail.com>
2025-12-26 23:08:45 +0530

    Added hit ratio and miss rate metrics to simulator output

--- src/simulator.js
+++ src/simulator.js
@@ -3,23 +3,35 @@ const simulateLRU = require("./lru");
 const simulateLFU = require("./lfu");
 const simulateARC = require("./arc");
 
+function addMetrics(stats) {
+    const totalAccesses = stats.hits + stats.misses;
+
+    return {
+        ...stats,
+        totalAccesses,
+        hitRatio: stats.hits / totalAccesses,
+        missRate: stats.misses / totalAccesses
+    };
+}
+
 function runSimulation(trace, cacheSize, algorithms) {
     const results = {};
 
     for (const algo of algorithms) {
         if (algo === "FIFO") {
-            results.FIFO = simulateFIFO(trace, cacheSize);
+            results.FIFO = addMetrics(simulateFIFO(trace, cacheSize));
         }
 
         if (algo === "LRU") {
-            results.LRU = simulateLRU(trace, cacheSize);
+            results.LRU = addMetrics(simulateLRU(trace, cacheSize));
         }
+
         if (algo === "LFU") {
-            results.LFU = simulateLFU(trace, cacheSize);
+            results.LFU = addMetrics(simulateLFU(trace, cacheSize));
         }
 
         if (algo === "ARC") {
-            results.ARC = simulateARC(trace, cacheSize);
+            results.ARC = addMetrics(simulateARC(trace, cacheSize));
         }
     }
 
--- test_simulator.js
+++ test_simulator.js
@@ -2,7 +2,6 @@ const runSimulation = require("./src/simulator");
 
 const trace = ["A", "B", "C", "A", "D", "B", "A"];
 const cacheSize = 3;
-
 const algorithms = ["FIFO", "LRU", "LFU", "ARC"];
 
 const result = runSimulation(trace, cacheSize, algorithms);
